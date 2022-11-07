ee0e7d70621af05c84c046c39e3e1d44b4f7b627
Udit <udit@eskaytech.com>
2022-11-08 01:35:01 +0530

    Remove fixed executable paths

--- oracle/scripts/report.sh
+++ oracle/scripts/report.sh
@@ -4,7 +4,7 @@
 # Load environment variables
 export $(egrep -v '^#' ../.env | xargs -d '\n')
 
-/home/udit/Work/txlabs/blockless-cli/dev-bin.js  function invoke report \
+bls function invoke report \
   --env STORAGE_ENDPOINT='$STORAGE_ENDPOINT' \
   --env STORAGE_ACCESS_TOKEN='$STORAGE_ACCESS_TOKEN' \
   --env RPC_NODE_ENDPOINT='$RPC_NODE_ENDPOINT' \
